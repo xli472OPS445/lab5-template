a1a01c50cd212c6ea348632b74bc0bbb6b49257a
xli472OPS445 <lavylabonte@gmail.com>
2024-11-07 18:41:43 -0500

    lab5a done

--- lab5a.py
+++ lab5a.py
@@ -2,18 +2,19 @@
 # Author ID: [xli472]
 
 def read_file_string(file_name):
-    f = open('file_name', 'w')
-    for name in f:
-        f.write(str(f) + '\n')
-    f.close() 
+    f = open(file_name, 'r')
+    read_file = f.read()
+    f.close()
+    return read_file
+     
 
 def read_file_list(file_name):
-    file_name = open('file_name', 'w')
-    for name in file_name:
-        file_name.write(str(file_name) + '\n')
-    file_name.close() 
+    f = open(file_name, 'r')
+    read_file = [line.strip() for line in f] 
+    f.close()
+    return read_file
 
 if __name__ == '__main__':
-    file_name = 'data.txt'
+    file_name = '/home/xli472/ops445/lab5-template/data.txt'
     print(read_file_string(file_name))
     print(read_file_list(file_name))
